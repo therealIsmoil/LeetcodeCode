792302b26f86199bdf6af5fd0b21f17063649d4f
i.xusanov <khusanov.ismoilbek@gmail.com>
2022-10-21 10:58:50 +0500

    Solution 2441 accepted

---
+++ src/Solution_2441.java
@@ -0,0 +1,18 @@
+public class Solution_2441 {
+    public int findMaxK(int[] nums) {
+        int temp = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[i] + nums[j] == 0) {
+                    if (temp < Math.abs(nums[i])) {
+                        temp = Math.abs(nums[i]);
+                    }
+                    break;
+                }
+            }
+        }
+
+        return temp;
+    }
+}
